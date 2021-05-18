a28b2b75c13e5f5b5ee0f4a28fff6791bf119b59
H0l1-0 <80547240+H0l1-0@users.noreply.github.com>
2021-05-18 06:56:02 -0700

    Update list.php

--- list.php
+++ list.php
@@ -3,71 +3,172 @@
     require_once('database.php') ;
 
     if (!isset($_POST['job'])) {
-       echo"<p>Please Choice a Job</p>" ;
+       echo"<p>Please Choose a Job</p>" ;
     }else{
         $job= filter_var($_POST['job'], FILTER_SANITIZE_STRING);
     
-        $query1 = "SELECT * FROM tasks WHERE job_id = 7068" ;
-        $query2 = "SELECT * FROM tasks WHERE job_id = 7071" ;
-        $query3 = "SELECT * FROM tasks WHERE job_id = 7079" ;
+        $sqlIncomplete1 = "SELECT task_name, start_date, end_date FROM tasks WHERE job_id = 7068 AND is_completed = 'no'" ;
+        $sqlIncomplete2 = "SELECT task_name, start_date, end_date FROM tasks WHERE job_id = 7071 AND is_completed = 'no'" ;
+        $sqlIncomplete3 = "SELECT task_name, start_date, end_date FROM tasks WHERE job_id = 7079 AND is_completed = 'no'" ;
 
-        $q1Result = mysqli_query($conn, $query1) ;
-        $q2Result = mysqli_query($conn, $query2) ;
-        $q3Result = mysqli_query($conn, $query3) ;
+        $q1Result = mysqli_query($conn, $sqlIncomplete1) ;
+        $q2Result = mysqli_query($conn, $sqlIncomplete2) ;
+        $q3Result = mysqli_query($conn, $sqlIncomplete3) ;
 
-        $NumFields1 = mysqli_num_fields($q1Result);
-        $NumFields2 = mysqli_num_fields($q2Result);
-        $NumFields3 = mysqli_num_fields($q3Result);
+
+         //Fetch all imcomplete list items
+        $incompleteItems1 = mysqli_fetch_all($q1Result);
+        $incompleteItems2 = mysqli_fetch_all($q2Result);
+        $incompleteItems3 = mysqli_fetch_all($q3Result);
+
+        //Get incomplete items
+        $sqlCompleted1 = "SELECT task_id, task_name, start_date, end_date FROM tasks WHERE job_id = 7068 AND is_completed = 'yes'" ;
+        $sqlCompleted2 = "SELECT task_id, task_name, start_date, end_date FROM tasks WHERE job_id = 7071 AND is_completed = 'yes'" ;
+        $sqlCompleted3 = "SELECT task_id, task_name, start_date, end_date FROM tasks WHERE job_id = 7079 AND is_completed = 'yes'" ;
+
+        $completeResult1 = mysqli_query($conn, $sqlIncomplete1) ;
+        $completeResult2 = mysqli_query($conn, $sqlIncomplete2) ;
+        $completeResult3 = mysqli_query($conn, $sqlIncomplete3) ;
+
+        //Fetch all completed items
+        $completeItems1 = mysqli_fetch_all($completeResult1);
+        $completeItems2 = mysqli_fetch_all($completeResult2);
+        $completeItems3 = mysqli_fetch_all($completeResult3);       
+
 
         // Disply query result
-        echo "<table width='100%' border='1'>" ;
-        echo "<tr><th>ID</th><th>Task ID</th><th>Task Name</th><th>Job ID</th><th>Start Date</th><th>End Date</th></tr>" ;
+        echo "<!doctype html>";
+        echo "<html lang='en'>";
+        echo "<head>";
+
+        echo "<meta charset='utf-8'>";
+        echo "<meta name='viewport' content='width=device-width, initial-scale=1'>";
+
+        echo "<title>Task To-Do List</title>";
+
+        echo "<link rel='stylesheet' href='//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css'>";
+        echo "<link rel='stylesheet' href='list.css'>";
+        echo "</head>";
+        echo "<body>";
+
+        echo "<p><h2 align='center'>Task To-Do List</h2></p>";
+        echo "<div class='li_containers'>";
+ 
+
         if ($job == 'WebDev') {
-            $Row = @mysqli_fetch_row($q1Result) ;
-            do {
-                echo "<tr>" ;
-                for ($i = 0 ; $i < $NumFields1 ; $i++) {
-                    echo "<td>{$Row[$i]}</td>" ;
-                }
-                echo "</tr>" ;
-                $Row = @mysqli_fetch_row($q1Result) ;
-            } while ($Row) ;
-            echo "</table>" ;
-            echo "<p>Please save the task ID number.</p>";
-            echo "<p>Submit the task ID number on the Hours section.</p>";
-            echo "<p>Note: If first time doing the task, Enter current data and time. An Email will be sent about the task details.</p>";
-        }
-        if ($job == 'Gadget') {
-            $Row = @mysqli_fetch_row($q2Result) ;
-            do {
-                echo "<tr>" ;
-                for ($i = 0 ; $i < $NumFields2 ; $i++) {
-                    echo "<td>{$Row[$i]}</td>" ;
-                }
-                echo "</tr>" ;
-                $Row = @mysqli_fetch_row($q2Result) ;
-            } while ($Row) ;
-            echo "</table>" ;
-            echo "<p>Please save the task ID number.</p>";
-            echo "<p>Submit the task ID number on the Hours section.</p>";
-            echo "<p>Note: If first time doing the task, Enter current data and time. An Email will be sent about the task details.</p>";
-        }
-        if ($job == 'Data') {
-            $Row = @mysqli_fetch_row($q3Result);
-            do {
-                echo "<tr>" ;
-                for ($i = 0 ; $i < $NumFields3 ; $i++) {
-                    echo "<td>{$Row[$i]}</td>" ;
-                }
-                echo "</tr>" ;
-                $Row = @mysqli_fetch_row($q3Result) ;
-            } while ($Row) ;
-            echo "</table>" ;
-            echo "<p>Please save the task ID number.</p>";
-            echo "<p>Submit the task ID number on the Hours section.</p>";
-            echo "<p>Note: If first time doing the task, Enter current data and time. An Email will be sent about the task details.</p>";
-        }
+
+            foreach ($incomleteItems1 as $key => $item) { 
+
+                echo "<div class='ui-widget-content listitems' data-itemid=".$item["task_id"].">";
     
+                echo "<p><strong><".$item['task_name']."</strong></p>";
+      
+                echo "<hr/>";
+      
+                echo "<p>".$item['start_date']."</p>";
+     
+                echo "<p>".$item['end_date']."</p>";
+      
+                echo "</div>";
+      
+            }
+            echo "</div>";
+
+            echo "<div id='droppable' class='ui-widget-header'>";
+            foreach ($completeItems1 as $key => $citem) {
+                echo "<div class='listitems'>";
+                echo "<p><strong>".$citem['task_name']."</p>";
+                echo "<hr/>";
+     
+                echo "<p>".$citem['start_date']."</p>";
+
+                echo "<p>".$citem['end_date']."</p>";
+                
+                echo "</div>";
+	        }
+            echo "</div>";
+        } 
+
+        if ($job == 'WebDev') {
+
+            foreach ($incomleteItems1 as $key => $item) { 
+
+                echo "<div class='ui-widget-content listitems' data-itemid=". $item["task_id"].">";
+  
+                echo "<p><strong><".$item['task_name']."</strong></p>";
+    
+                echo "<hr/>";
+    
+                echo "<p>".$item['start_date']."</p>";
+   
+                echo "<p>".$item['end_date']."</p>";
+    
+                echo "</div>";
+    
+            }
+            echo "</div>";
+
+            echo "<div id='droppable' class='ui-widget-header'>";
+            foreach ($completeItems1 as $key => $citem) {
+                echo "<div class='listitems'>";
+                echo "<p><strong>".$citem['task_name']."</p>";
+                echo "<hr/>";
+   
+                echo "<p>".$citem['start_date']."</p>";
+
+                echo "<p>".$citem['end_date']."</p>";
+              
+                echo "</div>";
+            }
+            echo "</div>";
+        } 
+
+        if ($job == 'WebDev') {
+
+            foreach ($incomleteItems1 as $key => $item) { 
+
+                echo "<div class='ui-widget-content listitems' data-itemid=". $item["task_id"].">";
+
+                echo "<p><strong><".$item['task_name']."</strong></p>";
+  
+                echo "<hr/>";
+  
+                echo "<p>".$item['start_date']."</p>";
+ 
+                echo "<p>".$item['end_date']."</p>";
+  
+                echo "</div>";
+  
+            }
+            echo "</div>";
+
+            echo "<div id='droppable' class='ui-widget-header'>";
+            foreach ($completeItems1 as $key => $citem) {
+               echo "<div class='listitems'>";
+               echo "<p><strong>".$citem['task_name']."</p>";
+               echo "<hr/>";
+ 
+               echo "<p>".$citem['start_date']."</p>";
+
+               echo "<p>".$citem['end_date']."</p>";
+            
+               echo "</div>";
+            }
+            echo "</div>";
+        } 
+
+        echo "<script src='https://code.jquery.com/jquery-1.12.4.js'></script>";
+        echo "<script src='https://code.jquery.com/ui/1.12.1/jquery-ui.js'></script>";
+        echo "<script src='list.js'></script>";
+
+        echo "</body>";
+        echo "</html>";
+
+    
+        //Free result set
+        mysqli_free_result($completeResult1);
+        mysqli_free_result($completeResult2);
+        mysqli_free_result($completeResult3);
         mysqli_free_result($q1Result);
         mysqli_free_result($q2Result);
         mysqli_free_result($q3Result);
